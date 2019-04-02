3da1a6b4f7df431211dee7b784c17afaaad036c1
Ricardo Baniski <ricardo.baniski@opet.edu.br>
2019-04-02 20:42:09 -0300

    Create 20190402c.py

---
+++ Opet/20190402c.py
@@ -0,0 +1,49 @@
+print('Exemplo 09')
+
+def sub(x,y):
+    r = x-y
+    return r
+print(sub(10,5))
+
+
+print('\nExemplo 10')
+
+def soma(x,y):
+    return x + y
+print(soma(10,5))
+
+
+print('\nExemplo 11')
+
+def minMax(v):
+    minimo= v[0]
+    maximo = v[0]
+    for elemento in v:
+        if elemento > maximo:
+            maximo = elemento
+        if elemento < minimo:
+            minimo = elemento
+    return minimo, maximo
+print(minMax([5,7,2,9,1]))    
+x,y = minMax([7,2,5,4])
+print(x)
+print(y)
+
+
+print('\nExemplo 12')
+
+def fat(n):
+      if n in (0,1):
+          return 1
+      return fat (n-1)*n
+print(fat(5))
+
+
+print('\nExemplo 13')
+
+def soma(x,y):
+      return x + y
+sl = lambda x,y: x + y
+print(soma(5,10))
+print(sl(5,10))
+    
