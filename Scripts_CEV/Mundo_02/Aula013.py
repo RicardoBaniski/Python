b414d4207de2fec088eb55fc40b7b73f2531c5f9
Ricardo Baniski <ricardo.baniski@opet.edu.br>
2019-04-16 18:43:56 -0300

    Laços de repetição
    FOR

---
+++ Scripts_CEV/Mundo_02/Aula013.py
@@ -0,0 +1,11 @@
+for c in range(1, 6):
+    print('Oi', c)
+print('FIM\n')
+
+for c in range(6, 0, -1):
+    print('Oi', c)
+print('FIM\n')
+
+for c in range(1, 6, 2):
+    print('Oi', c)
+print('FIM')
