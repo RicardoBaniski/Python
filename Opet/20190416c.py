0629fb6cf2dee059f4f3dbd85f50b7fef287d3fc
Ricardo Baniski <ricardo.baniski@opet.edu.br>
2019-04-16 21:04:38 -0300

    Update 20190416c.py

--- Opet/20190416c.py
+++ Opet/20190416c.py
@@ -1,4 +1,3 @@
-# SOBRECARGA DE MÉTODO
 class Pessoa:
     def __init__(self, nome, idade):
         self.nome = nome
@@ -21,11 +20,13 @@ f1 = Funcionario('A', 37, 1500.00)
 f2 = Funcionario('B', 22, 1200.00)
 f3 = Funcionario('C', 40, 3000.00)
 
-#print(f1 + f2 + f3)
+# print(f1 + f2 + f3)
 lista = [f1, f2, f3]
-
-[print(x.nome) for x in lista]
+[print(x.nome) for x in lista]  # LIST COMPREMENSION
+print('')
 
 lista.sort()
+[print(x.nome) for x in lista]  # LIST COMPREMENSION
+print('')
 
-[print(x.nome) for x in lista]
+print(sum(lista))
